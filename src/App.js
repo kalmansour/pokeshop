57631332d70234b3cbc51bc43542b5a607b74502
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-02-16 10:57:57 +0300

    feat: dark mode
    A dark mode button that changes app theme color.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
 
 // Components
 import Home from "./components/Home";
 import PokemonList from "./components/PokemonList";
 
+// Styling
+import { GlobalStyle, theme, ThemeButton } from "./styles";
+
 function App() {
-  let counter = 0;
-  const handleIncrement = (name) => alert(`Hello ${name}`);
+  const [currentTheme, setCurrentTheme] = useState("light");
+  const [currentThemeName, setCurrentThemeName] = useState("Dark Mode");
+
+  const toggleTheme = () => {
+    setCurrentTheme(currentTheme === "light" ? "dark" : "light");
+    setCurrentThemeName(currentTheme === "light" ? "Light Mode" : "Dark Mode");
+  };
+
   return (
     <div>
-      <button onClick={() => handleIncrement("Khaled")}>Increment</button>
-      {counter}
-      <button>Decrement</button>
-      <Home />
-      <PokemonList />
+      <ThemeProvider theme={theme[currentTheme]}>
+        <GlobalStyle />
+        <ThemeButton onClick={toggleTheme}>{currentThemeName}</ThemeButton>
+        <Home />
+        <PokemonList />
+      </ThemeProvider>
     </div>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,5 @@
+import styled, { createGlobalStyle } from "styled-components";
+
 export const styles = {
   text: { textAlign: "center" },
   pokeshopImage: {
@@ -18,4 +20,32 @@ export const styles = {
   pokemon: { margin: "20px" },
 };
 
+export const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${({ theme }) => theme.mainColor};
+    background-color: ${({ theme }) => theme.backgroundColor}
+  }
+`;
+
+export const theme = {
+  light: {
+    mainColor: "#242424", // main font color
+    backgroundColor: "#fefafb", // main background color
+    pink: "#ff85a2",
+  },
+  dark: {
+    mainColor: "#fefafb", // main font color
+    backgroundColor: "#242424", // main background color
+    pink: "#ff85a2",
+  },
+};
+
+export const ThemeButton = styled.button`
+  font-size: 1em;
+  margin: 1.25em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  background-color: ${({ theme }) => theme.mainColor};
+  color: ${({ theme }) => theme.backgroundColor};
+`;
 export default styles;
