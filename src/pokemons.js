2861856e04d8623eed494a4b3ab6dcb2a0cd56e8
kalmansour <63953899+kalmansour@users.noreply.github.com>
2022-06-12 16:38:47 +0300

    chore: add rarity attribute
    Now we can know whether a pokemon is rare or not.

--- src/pokemons.js
+++ src/pokemons.js
@@ -5,6 +5,7 @@ const pokemonsData = [
     description: "grass type pokemon",
     price: "10",
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    isRare: true,
   },
   {
     id: 2,
@@ -12,6 +13,7 @@ const pokemonsData = [
     description: "fire type pokemon",
     price: "10",
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    isRare: true,
   },
   {
     id: 3,
@@ -19,6 +21,7 @@ const pokemonsData = [
     description: "water type pokemon",
     price: "10",
     image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    isRare: true,
   },
 ];
 
